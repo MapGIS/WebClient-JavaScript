ce6f0c321ca1a407894fb632481a27249f27c358
killer1992 <killer1992@yeah.net>
2021-04-30 17:18:19 +0800

    【SDK】【新增】【杨琨】【Arcgis要素服务接口】

---
+++ src/service/ArcGis/index.js
@@ -0,0 +1,34 @@
+/**
+ *@module ArcGis
+ */
+
+import { ArcGisFeatureLayer } from './FeatureLayer';
+import { ArcGisFindParameters } from './FindParameters';
+import { ArcGisFindTask  } from './FindTask';
+import { ArcGisIdentifyTask  } from './IdentifyTask';
+import { ArcGisIdentifyParameters  } from './IdentifyParameters';
+import { ArcGisGeometry  } from './Geometry';
+import { ArcGisQuery  } from './Query';
+import { ArcGisQueryTask  } from './QueryTask';
+import { ArcGisPolygon  } from './Polygon';
+import { ArcGisPoint  } from './Point';
+import { ArcGisMultipoint  } from './Multipoint';
+import { ArcGisPolyline  } from './Polyline';
+import { ArcGisGraphic  } from './Graphic';
+import { ArcGisExtent  } from './Extent';
+
+export {
+    ArcGisFeatureLayer,
+    ArcGisFindParameters,
+    ArcGisFindTask,ArcGisIdentifyTask,
+    ArcGisIdentifyParameters,
+    ArcGisGeometry,
+    ArcGisQuery,
+    ArcGisQueryTask,
+    ArcGisPoint,
+    ArcGisMultipoint,
+    ArcGisPolyline,
+    ArcGisPolygon,
+    ArcGisGraphic,
+    ArcGisExtent
+};
