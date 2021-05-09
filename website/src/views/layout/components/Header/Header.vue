260f0e542a283d350cab46af3317ddaa7cd08a1b
潘卓然ParnDeedlit <panzhuoran.parndeedlit@mapgis.com>
2021-05-09 15:06:01 +0800

    【站点】【样式】【实现新版的暗色样式细节】

--- website/src/views/layout/components/Header/Header.vue
+++ website/src/views/layout/components/Header/Header.vue
@@ -1,165 +1,126 @@
 <template>
-  <div :class="{'mapgis-header': true, 'mapgis-header-mobile': mobile}">
-    <router-link to="/">
-      <div class="mapgis-webclient-header">
-        <img
-          :src="logo"
-          class="mapgis-webclient-logo"
-        >
-        <span
-          v-if="!mobile"
-          class="mapgis-webclient-text"
-        >
-          Client for JavaScript
-        </span>
-      </div>
-    </router-link>
-    <IconFont
-        :style="style"
-        type="iconicon_commonly_barmenu"
-        class="mapgis-webclient-menu-icon"
-        @click="isShowMenu = !isShowMenu"
-    />
-    <div :class="['mapgis-webclient-menu',{'is-show':isShowMenu}]">
-        <div class="mapgis-webclient-nav">
-        <el-popover
-        v-for="(h,i) in mobile ? mobileHeaders : headers"
-        :key="i"
-        placement="top-start"
-        trigger="hover"
-      >
-        <header-menu
-          :menus="h.menus"
-          :icon="h.icon"
-        />
-        <el-button
-          type="text"
-          slot="reference"
-          :class="{'active': isActiveMenu(h.title)}"
-        >
-          <IconFont
-            :type="h.icon"
-            class="menu-icon"
-          />
-          {{h.title}}
-        </el-button>
-      </el-popover>
-      <div v-if="mobile">
-        <el-popover
-          v-for="h in mobileSubheaders"
-          :key="h.title"
-          placement="top-start"
-          trigger="hover"
-        >
-          <header-sub-menu
-            :active="activeTabs[h.title]"
-            :menus="h.menus"
-          />
-          <el-button
-            type="text"
-            slot="reference"
-          >{{h.title}}</el-button>
-        </el-popover>
-      </div>
+    <div :class="{ 'mapgis-header': true, 'mapgis-header-mobile': mobile }">
+        <router-link to="/">
+            <div class="mapgis-webclient-header">
+                <img :src="logo" class="mapgis-webclient-logo" />
+                <span v-if="!mobile" class="mapgis-webclient-text"> Client for JavaScript </span>
+            </div>
+        </router-link>
+        <IconFont :style="style" type="iconicon_commonly_barmenu" class="mapgis-webclient-menu-icon" @click="isShowMenu = !isShowMenu" />
+        <div :class="['mapgis-webclient-menu', { 'is-show': isShowMenu }]">
+            <div class="mapgis-webclient-nav">
+                <el-popover v-for="(h, i) in mobile ? mobileHeaders : headers" :key="i" placement="top-start" trigger="hover">
+                    <header-menu :menus="h.menus" :icon="h.icon" />
+                    <el-button type="text" slot="reference" :class="{ active: isActiveMenu(h.title) }">
+                        <IconFont :type="h.icon" class="menu-icon" />
+                        {{ h.title }}
+                    </el-button>
+                </el-popover>
+                <div v-if="mobile">
+                    <el-popover v-for="h in mobileSubheaders" :key="h.title" placement="top-start" trigger="hover">
+                        <header-sub-menu :active="activeTabs[h.title]" :menus="h.menus" />
+                        <el-button type="text" slot="reference">{{ h.title }}</el-button>
+                    </el-popover>
+                </div>
+            </div>
         </div>
     </div>
-  </div>
 </template>
 
 <script>
-import { isMobile } from "@/utils/mobile";
+import { isMobile } from '@/utils/mobile';
 import HeaderMenu from './Menu';
 import HeaderSubMenu from './SubMenu';
 import axios from 'axios';
-import IconFont from "@/components/IconFont/iconfront";
+import IconFont from '@/components/IconFont/iconfront';
 
 export default {
-  components: {
-    IconFont,
-    HeaderMenu,
-    HeaderSubMenu,
-  },
-  data () {
-    return {
-      mobile: isMobile(),
-      logo: './static/assets/logo/mapgis_blue.png',
-      activeMenu: '',
-      activeTabs: {},
-      urls: ['config-headers.json', 'config-headers-sub.json', 'config-headers-mobile.json', 'config-headers-sub-mobile.json'],
-      headers: [],
-      subheaders: [],
-      mobileHeaders: [],
-      mobileSubheaders: [],
-      isShowMenu: false,
-      style: {
-          fontSize: '24px',
-          color: '#FFFFFF',
-          lineHeight: '72px'
-      }
-    };
-  },
-  created () {
-    const vm = this;  
-    const { mobileSubheaders, subheaders } = this;
-    const headers = isMobile() ? mobileSubheaders : subheaders;
-    headers.forEach((h) => {
-      this.activeTabs[h.title] = h.active ? h.active.toLowerCase() : undefined;
-    });
-    let nets = this.urls.map(url => {
-      return new axios(`../static/demo/config/${url}`)
-    });
-    Promise.all(nets).then(res => {
-      vm.headers = res[0].data;
-      vm.subheaders = res[1].data;
-      vm.mobileHeaders = res[2].data;
-      vm.mobileSubheaders = res[3].data;
-    });
-  },
-  mounted () {
-    let mapmode = this.$route.params.mapmode;
-    let active = mapmode ? mapmode.toLowerCase() : undefined;
-    this.activeMenu = active;
-  },
-  watch: {
-    '$route' (to, from) {
-      // 对路由变化作出响应...
-      const { mobileSubheaders, subheaders } = this;
-      let mapmode = to.params.mapmode
-      if (mapmode !== from.params.mapmode) {
-        const headers = this.mobile ? mobileSubheaders : subheaders;
-        this.activeMenu = mapmode.toLowerCase();
-        headers.forEach(header => {
-          header.menus.forEach(m => {
-            let active = m.title.toLowerCase();
-            if (active === mapmode) {
-              this.activeTabs[header.title] = active
+    components: {
+        IconFont,
+        HeaderMenu,
+        HeaderSubMenu
+    },
+    data() {
+        return {
+            mobile: isMobile(),
+            logo: './static/assets/logo/mapgis_blue.png',
+            activeMenu: '',
+            activeTabs: {},
+            urls: ['config-headers.json', 'config-headers-sub.json', 'config-headers-mobile.json', 'config-headers-sub-mobile.json'],
+            headers: [],
+            subheaders: [],
+            mobileHeaders: [],
+            mobileSubheaders: [],
+            isShowMenu: false,
+            style: {
+                fontSize: '24px',
+                color: '#FFFFFF',
+                lineHeight: '72px'
             }
-          })
+        };
+    },
+    created() {
+        const vm = this;
+        const { mobileSubheaders, subheaders } = this;
+        const headers = isMobile() ? mobileSubheaders : subheaders;
+        headers.forEach((h) => {
+            this.activeTabs[h.title] = h.active ? h.active.toLowerCase() : undefined;
         });
-      }
+        let nets = this.urls.map((url) => {
+            return new axios(`../static/demo/config/${url}`);
+        });
+        Promise.all(nets).then((res) => {
+            vm.headers = res[0].data;
+            vm.subheaders = res[1].data;
+            vm.mobileHeaders = res[2].data;
+            vm.mobileSubheaders = res[3].data;
+        });
     },
-  },
-  methods: {
-    isActiveTab (title) {
-      title = title.toLowerCase()
-      Object.keys(this.activeTabs).forEach(key => {
-        let active = this.activeTabs[key]
-        if (title.indexOf(active) >= 0) {
-          return true;
+    mounted() {
+        let mapmode = this.$route.params.mapmode;
+        let active = mapmode ? mapmode.toLowerCase() : undefined;
+        this.activeMenu = active;
+    },
+    watch: {
+        $route(to, from) {
+            // 对路由变化作出响应...
+            const { mobileSubheaders, subheaders } = this;
+            let mapmode = to.params.mapmode;
+            if (mapmode !== from.params.mapmode) {
+                const headers = this.mobile ? mobileSubheaders : subheaders;
+                this.activeMenu = mapmode.toLowerCase();
+                headers.forEach((header) => {
+                    header.menus.forEach((m) => {
+                        let active = m.title.toLowerCase();
+                        if (active === mapmode) {
+                            this.activeTabs[header.title] = active;
+                        }
+                    });
+                });
+            }
         }
-      })
-      return false;
     },
-    isActiveMenu (title) {
-      if (!this.activeMenu || !title) return false
-      title = title.toLowerCase();
-      if (title.indexOf(this.activeMenu) >= 0) {
-        return true
-      }
-      return false
+    methods: {
+        isActiveTab(title) {
+            title = title.toLowerCase();
+            Object.keys(this.activeTabs).forEach((key) => {
+                let active = this.activeTabs[key];
+                if (title.indexOf(active) >= 0) {
+                    return true;
+                }
+            });
+            return false;
+        },
+        isActiveMenu(title) {
+            if (!this.activeMenu || !title) return false;
+            title = title.toLowerCase();
+            if (title.indexOf(this.activeMenu) >= 0) {
+                return true;
+            }
+            return false;
+        }
     }
-  }
-}
+};
 </script>
 
 <style lang="scss">
@@ -187,12 +148,12 @@ export default {
     font-family: Microsoft YaHei;
     width: 100%;
     padding: 0px !important;
-    height: 72px;
+    height: 64px;
     background: rgba(37, 45, 69, 1);
 
     .mapgis-webclient-header {
         width: fit-content;
-        height: 72px;
+        height: 64px;
         align-items: center;
         float: left;
         display: flex;
@@ -207,12 +168,14 @@ export default {
             height: 24px;
             margin-left: 13px;
             color: rgba(255, 255, 255, 1);
-            line-height: 24px;
-            font-size: 22px;
-            font-weight: bold;
-            font-style: italic;
             font-stretch: normal;
             letter-spacing: 1.5px;
+            font-size: 22px;
+            font-family: Microsoft YaHei;
+            font-weight: bold;
+            font-style: italic;
+            color: #ffffff;
+            line-height: 24px;
         }
         .menu-icon {
             margin-right: 8px !important;
@@ -221,7 +184,7 @@ export default {
 
     .mapgis-webclient-menu-icon {
         float: right;
-        height: 72px;
+        height: 64px;
         margin-right: 48px;
         cursor: pointer;
         display: none;
@@ -245,17 +208,17 @@ export default {
         @media screen and (max-width: 1220px) {
             width: 100%;
             flex-wrap: wrap;
-            background-color: rgba(37, 45, 69, .5);
+            background-color: rgba(37, 45, 69, 0.5);
             display: none;
 
             &.is-show {
-            display: flex;
+                display: flex;
             }
         }
 
         span {
             width: fit-content;
-            height: 72px;
+            height: 64px;
             // margin: 0px 6px;
             padding: 0px 4px;
             // margin-let: 12.5px;
@@ -264,8 +227,8 @@ export default {
             font-weight: 500;
             text-align: center;
             color: rgba(255, 255, 255, 1);
-            line-height: 72px;
-            
+            line-height: 64px;
+
             .el-button {
                 padding: 0;
                 border-width: 0;
