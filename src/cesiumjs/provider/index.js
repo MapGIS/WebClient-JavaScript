14931342b5a7a53b7ed65a010ba2e25c34652df3
潘卓然ParnDeedlit <panzhuoran.parndeedlit@mapgis.com>
2021-07-02 15:21:53 +0800

    【SDK】【Cesium】【新增Cesium-ArcGIS-reversebbox瓦片】

--- src/cesiumjs/provider/index.js
+++ src/cesiumjs/provider/index.js
@@ -1,3 +1,4 @@
-export { IgsTileProvider } from "./IgsTileProvider";
-export { IgsDocProvider } from "./IgsDocProvider";
-export { WmtsTileProvider } from "./WmtsTileProvider";
+export { IgsTileProvider } from './IgsTileProvider';
+export { IgsDocProvider } from './IgsDocProvider';
+export { WmtsTileProvider } from './WmtsTileProvider';
+export { WebReverseMapServiceImageryProvider } from './WebReverseMapServiceImageryProvider';
