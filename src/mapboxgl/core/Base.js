74667e954e9badf767e1976be413b4e6301ec574
潘卓然ParnDeedlit <panzhuoran.parndeedlit@mapgis.com>
2021-10-14 14:54:41 +0800

    【SDK】【MapboxGL】【修复全局MapboxGL依赖】

--- src/mapboxgl/core/Base.js
+++ src/mapboxgl/core/Base.js
@@ -4,12 +4,9 @@
  */
 import mapboxgl from '@mapgis/mapbox-gl';
 
-export var Zondy = (window.Zondy = window.Zondy || {})
-
-Zondy.Util = Zondy.Util || {}
-Zondy.Network = Zondy.Network || {}
-
-Zondy.Event = Zondy.Event || {}
-Zondy.Socket = Zondy.Socket || {}
-
-mapboxgl.zondy = mapboxgl.zondy || {}
+window.mapboxgl = mapboxgl;
+mapboxgl.Zondy = window.mapboxgl.Zondy || {};
+var Zondy = window.mapboxgl.Zondy || {};
+Zondy.Map = Zondy.Map || {};
+export { Zondy };
+export { mapboxgl };
