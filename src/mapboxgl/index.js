504341409de1d8316f327e6b7c35f3d0e5b8fef2
mayuanye2008 <mayuanye2008@163.com>
2022-03-05 01:42:32 +0800

    1.修订一张图中统计专题引入GraphThemeLayer类时报错的问题。 2.更新webclient-es6-service、webclient-mapboxgl所依赖的core.js的版本，以适应高版本的core.js

--- src/mapboxgl/index.js
+++ src/mapboxgl/index.js
@@ -1,37 +1,37 @@
 import { MapExtend } from './MapExtend';
 export { MapExtend };
 
-import { Common, BaseServer, Extend, MRFS, MRGS, MRFWS , MRMS, MRCS, Info, PostGIS, CloudDisk } from '@mapgis/webclient-es6-service';
-export { Common, BaseServer, Extend, MRFS, MRGS, MRFWS , MRMS, MRCS, Info, PostGIS, CloudDisk };
+import { Common, BaseServer, Extend, MRFS, MRGS, MRFWS, MRMS, MRCS, Info, PostGIS, CloudDisk } from '@mapgis/webclient-es6-service';
+export { Common, BaseServer, Extend, MRFS, MRGS, MRFWS, MRMS, MRCS, Info, PostGIS, CloudDisk };
 
 import { mapboxgl, MapDocLayer, MapTileLayer, MapWmsLayer, MapWMTSLayer, MapVectorLayer, ArcGISLayer, TDTLayer } from './layer';
 export { mapboxgl, MapDocLayer, MapTileLayer, MapWmsLayer, MapWMTSLayer, MapVectorLayer, ArcGISLayer, TDTLayer };
 
-// import {
-//     GeoFeatureThemeLayer,
-//     ThemeLayer,
-//     RangeThemeLayer,
-//     UniqueThemeLayer,
-//     GraphThemeLayer,
-//     graphThemeLayer,
-//     RandomThemeLayer,
-//     SimpleThemeLayer,
-//     RankSymbolThemeLayer,
-//     ThemeStyle
-// } from './theme';
+import {
+    GeoFeatureThemeLayer,
+    ThemeLayer,
+    RangeThemeLayer,
+    UniqueThemeLayer,
+    GraphThemeLayer,
+    graphThemeLayer,
+    RandomThemeLayer,
+    SimpleThemeLayer,
+    RankSymbolThemeLayer,
+    ThemeStyle
+} from './theme';
 
-// export {
-//     GeoFeatureThemeLayer,
-//     ThemeLayer,
-//     RangeThemeLayer,
-//     UniqueThemeLayer,
-//     GraphThemeLayer,
-//     graphThemeLayer,
-//     RandomThemeLayer,
-//     SimpleThemeLayer,
-//     RankSymbolThemeLayer,
-//     ThemeStyle
-// };
+export {
+    GeoFeatureThemeLayer,
+    ThemeLayer,
+    RangeThemeLayer,
+    UniqueThemeLayer,
+    GraphThemeLayer,
+    graphThemeLayer,
+    RandomThemeLayer,
+    SimpleThemeLayer,
+    RankSymbolThemeLayer,
+    ThemeStyle
+};
 
 import {
     EchartsLayer,
