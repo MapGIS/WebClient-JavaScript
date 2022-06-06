3d08d8ecf8708216985c2f72f3a163fca0b1e6c6
killer1992 <killer1992@yeah.net>
2022-06-06 20:36:02 +0800

    【杨琨】【添加二三维联动-绘制功能】

--- src/service/2DPlot/Draw/DrawPolyline2D.js
+++ src/service/2DPlot/Draw/DrawPolyline2D.js
@@ -12,11 +12,11 @@ import {addExtendLayersPlot} from "../../3DPlot/Utils/PlotUtil";
 
 const _ = require('lodash');
 export default class DrawPolyline2D extends DrawObject {
-    constructor(fabricCanvas, symbol, options) {
+    constructor(plotLayer, symbol, options) {
         super();
-        this.m_fabricCanvas = fabricCanvas;
+        this._plotLayer = plotLayer;
         this.m_symbol = symbol;
-        this.m_coordSys = this.m_fabricCanvas.getCoordSys();
+        this.m_coordSys = this._plotLayer.getCoordSys();
         this.m_object = null;
         this.m_coords = [[0, 0]];
         this.onMouseUp = this.innerOnMouseUp.bind(this);
@@ -28,17 +28,17 @@ export default class DrawPolyline2D extends DrawObject {
 
     addHooks() {
         super.addHooks();
-        this.m_fabricCanvas.on('mouse:move', this.onMouseMove);
-        this.m_fabricCanvas.on('mouse:up', this.onMouseUp);
-        this.m_fabricCanvas.interactive = false;
+        this._plotLayer.on('mouse:move', this.onMouseMove);
+        this._plotLayer.on('mouse:up', this.onMouseUp);
+        this._plotLayer.interactive = false;
     }
 
     removeHooks() {
         this.m_object = null;
-        this.m_fabricCanvas.interactive = true;
-        this.m_fabricCanvas.off('mouse:move', this.onMouseMove);
-        this.m_fabricCanvas.off('mouse:up', this.onMouseUp);
-        this.m_fabricCanvas._isDrawing = false;
+        this._plotLayer.interactive = true;
+        this._plotLayer.off('mouse:move', this.onMouseMove);
+        this._plotLayer.off('mouse:up', this.onMouseUp);
+        this._plotLayer._isDrawing = false;
         super.removeHooks();
     }
 
@@ -47,7 +47,7 @@ export default class DrawPolyline2D extends DrawObject {
         this.m_coords[this.m_coords.length - 1] = new Point(pnt[0], pnt[1]);
         if (this.m_coords.length >= 2 && this.m_object) {
             this.m_object.setPnts(this.m_coords);
-            this.m_fabricCanvas.requestRenderAll();
+            this._plotLayer.requestRenderAll();
         }
     }
 
@@ -71,15 +71,15 @@ export default class DrawPolyline2D extends DrawObject {
                 this.m_symbol.getElement().then((element) => {
                     this.m_object = PlotObjectFactory.createInstance(this.m_symbol.type, {
                         element: element,
-                        canvas: this.m_fabricCanvas
+                        canvas: this._plotLayer
                     });
-                    this.m_fabricCanvas.addPlot(this.m_object);
-                    this.m_fabricCanvas._isDrawing = true;
-                    addExtendLayersPlot(this.m_fabricCanvas._linkTool, this.m_object);
+                    this._plotLayer.addPlot(this.m_object);
+                    this._plotLayer._isDrawing = true;
+                    addExtendLayersPlot(this._plotLayer._linkTool, this.m_object);
                 });
             }
         }
 
-        this.m_fabricCanvas.requestRenderAll();
+        this._plotLayer.requestRenderAll();
     }
 }
