9e1a2e26192bd162ea23ee21ba96e507cde349e6
gongyuejian <gongyuejian.000@mapgis.com>
2022-06-13 20:31:56 +0800

    更新要素单圈缓冲分析示例

--- src/service/Igserver/MRFWS/FeatureBuffBySingleRing.js
+++ src/service/Igserver/MRFWS/FeatureBuffBySingleRing.js
@@ -1,9 +1,5 @@
-﻿import {
-    Zondy
-} from '../../common/Base';
-import {
-    FeatureBuffBase
-} from "./FeatureBuffBase";
+﻿import { Zondy } from '../../common/Base';
+import { FeatureBuffBase } from './FeatureBuffBase';
 
 /**
  * 要素缓冲分析（单圈）
@@ -16,55 +12,53 @@ import {
  * @param {Number} [option.rightRad = 0.001] 缓冲分析右半径
  * @example
  //初始化Zondy.Object.FeatureGeometry对象
- var regGeo = new Zondy.Object.FeatureGeometry();
- //设置区要素的空间几何信息
- var gReg = new Zondy.Object.GRegion([
- new Zondy.Object.AnyLine([new Zondy.Object.Arc([
- new Zondy.Object.Point2D(0.46, 30.1),
- new Zondy.Object.Point2D(11.48, 6.22),
- new Zondy.Object.Point2D(36.73, 7.6),
- new Zondy.Object.Point2D(58.77, 25.51),
- new Zondy.Object.Point2D(41.33, 49.39) ]) ])]);
- regGeo.setRegGeom([gReg]);
- //设置属性结构
- var regAttStr = new Zondy.Object.CAttStruct({
-                    FldName: ["ID", "面积", "周长", "LayerID"],
-                    FldNumber: 4,
-                    FldType: ["FldLong", "FldDouble", "FldDouble", "FldLong"]
-                });
- //实例化CAttDataRow类
- var values = [0, 62.566714, 50.803211, 0];
- var valuesRow = new Zondy.Object.CAttDataRow(values, 1);
- //实例化FeatureBuffBySingleRing类，设置要素缓冲分析必要参数，输出分析结果到缓冲分析结果图层
- var featureBufBySR = new Zondy.Service.FeatureBuffBySingleRing({
-                    //IGServer所在ip地址
-                    ip: "develop.smaryun.com",
-                    //IGServer请求端口号
-                    port: "6163",
-                    //设置要素缓冲分析左半径
-                    leftRad: 5,
-                    //设置要素缓冲分析右半径
-                    rightRad: 5
-                });
- //设置缓冲分析参数
- //设置几何信息
- featureBufBySR.sfGeometryXML = JSON.stringify([regGeo]);
- //设置属性结构
- featureBufBySR.attStrctXML = JSON.stringify(regAttStr);
- //设置属性值
- featureBufBySR.attRowsXML = JSON.stringify([valuesRow]);
- //设置追踪半径
- featureBufBySR.traceRadius = 0.0001;
- //设置缓冲结果的名称以及存放地址
- var resultname = "singleBuffAnalysisResultLayer" + getCurentTime();
- //缓存结果图层的基地址
- var resultBaseUrl = "gdbp://MapGisLocal/OpenLayerVecterMap/sfcls/";
- featureBufBySR.resultName = resultBaseUrl + resultname;
- featureBufBySR.execute(function (res) {
-                    console.log(res);
-                }, "get",function (error) {
-                    console.log(error);
-                });
+            var regGeo = new Zondy.Object.FeatureGeometry();
+            //设置区要素的空间几何信息
+            var gReg = new Zondy.Object.GRegion([
+                new Zondy.Object.AnyLine([new Zondy.Object.Arc([
+                    new Zondy.Object.Point2D(0.46, 30.1),
+                    new Zondy.Object.Point2D(11.48, 6.22),
+                    new Zondy.Object.Point2D(36.73, 7.6),
+                    new Zondy.Object.Point2D(58.77, 25.51),
+                    new Zondy.Object.Point2D(41.33, 49.39),
+                    new Zondy.Object.Point2D(0.46, 30.1)
+                ])
+                ])
+            ]);
+            //设置区要素几何信息的方法。
+            regGeo.setRegGeom([gReg]);
+            //实例化CAttStruct类
+            var regAttStr = new Zondy.Object.CAttStruct({
+                FldName: ["ID", "面积", "周长", "LayerID"],
+                FldNumber: 4,
+                FldType: ["FldLong", "FldDouble", "FldDouble", "FldLong"]
+            });
+            var values = [1, 0.00058032464704422, 0.132101984752282, 8];
+            //创建属性信息对象
+            var valuesRow = new Zondy.Object.CAttDataRow(values, 3286);
+            //实例化FeatureBuffByMultiplyRing类，设置要素缓冲分析必要参数，输出分析结果到缓冲分析结果图层
+            var featureBufByMR = new Zondy.Service.FeatureBuffByMultiplyRing({
+                ip: "develop.smaryun.com",
+                port: "6163",    //访问IGServer的端口号，.net版为6163，Java版为8089,
+                //设置多圈缓冲分析的缓冲半径字符串
+                radiusStr: "2,4,6"
+            });
+            featureBufByMR.sfGeometryXML = JSON.stringify([regGeo]);
+            featureBufByMR.attStrctXML = JSON.stringify(regAttStr);
+            featureBufByMR.attRowsXML = JSON.stringify([valuesRow]);
+            featureBufByMR.traceRadius = 0.0001;
+
+            var resultname = "multiBuffResultLayer" + getCurentTime();
+            var resultBaseUrl = "gdbp://MapGisLocal/OpenLayerVecterMap/sfcls/"; //缓存结果图层的基地址
+            featureBufByMR.resultName = resultBaseUrl + resultname;
+            //调用Zondy.Service.AnalysisBase基类的execute方法执行要素缓冲分析，AnalysisSuccess为回调函数。
+            featureBufByMR.execute( res => {
+              console.log(res);
+            },
+            "post",
+            error => {
+              console.log(res);
+            });
  */
 class FeatureBuffBySingleRing extends FeatureBuffBase {
     constructor(option) {
@@ -96,10 +90,8 @@ class FeatureBuffBySingleRing extends FeatureBuffBase {
          * @description 工作流ID
          * @default "600238"
          */
-        this.flowID = "600238";
+        this.flowID = '600238';
     }
 }
-export {
-    FeatureBuffBySingleRing
-};
+export { FeatureBuffBySingleRing };
 Zondy.Service.FeatureBuffBySingleRing = FeatureBuffBySingleRing;
