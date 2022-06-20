91f7e266013800caa4d31549a2a0b120fcd9fe80
killer1992 <killer1992@yeah.net>
2022-06-20 20:56:13 +0800

    【SDK】【行业标绘数据转换】

--- src/service/PlotBase/SymbolManager/SymbolManager.js
+++ src/service/PlotBase/SymbolManager/SymbolManager.js
@@ -21,15 +21,15 @@ export default class SymbolManager {
 
     const {fontURL = '', baseUrl = ''} = options;
 
-    if(!window._mapgisSymanagerConfig_) {
+    if (!window._mapgisSymanagerConfig_) {
       window._mapgisSymanagerConfig_ = {};
     }
 
-    if(fontURL) {
+    if (fontURL) {
       window._mapgisSymanagerConfig_._fontURL = fontURL;
     }
 
-    if(baseUrl) {
+    if (baseUrl) {
       window._mapgisSymanagerConfig_._baseUrl = baseUrl;
     }
 
@@ -41,14 +41,49 @@ export default class SymbolManager {
     return SymbolManager.instance;
   }
 
+  formatData(data) {
+    const {symbols} = data;
+    let result = [];
+    for (let i = 0; i < symbols.length; i++) {
+      result.push({
+        title: symbols[i].name,
+        children: []
+      });
+      const {items} = symbols[i];
+      for (let j = 0; j < items.length; j++) {
+        if (items[j].type === 'folder') {
+          result[i].children.push({
+            type: items[j].name,
+            children: []
+          });
+          let icons = items[j].items;
+          for (let k = 0; k < icons.length; k++) {
+            result[i].children[j].children.push({
+              "id": icons[k].id,
+              "name": icons[k].name,
+              "type": icons[k].type,
+              "src": icons[k].path
+            });
+          }
+        }
+      }
+    }
+
+    return {
+      name: data.name,
+      children: result
+    };
+  }
+
   async getSymbols() {
     if (this._symbols) return this._symbols;
 
     try {
       const res = await axios.get(this._symbolsUrl);
       this._symbols = new SymbolCatalog();
-      this._symbols.fromJson(res.data);
+      this._symbols.fromJson(this.formatData(res.data));
       this._leaves = this._symbols.getAllLeaves();
+      this._symbols._config = res.data;
       return this._symbols;
     } catch (err) {
       const res = await getAction("/symbol/catalog/queryTreeListByPid");
